b7e934f7ddaf2614cd871394ec320f185652fad2
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-11 10:41:57 +0300

    feat: add header for mobile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+// import Main from '../Main/Main';
 
 function App() {
   return (
-    <div className="app">
+    <body className="app">
       <Header />
-    </div>
+    </body>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderLogoWhite from '../../images/Logo-white.svg';
+// import HeaderLogoBlack from '../../images/Logo-black.svg';
 
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" alt="" />
-      <p>Header</p>
+      <div className="header__container">
+        <img className="header__logo" alt="" src={HeaderLogoWhite} />
+        <div className="header__burger-button header__burger-button_active" />
+      </div>
     </header>
 
   );
