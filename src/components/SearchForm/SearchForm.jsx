49a62e8522b3e7f7d24c83c048485864e84d5d36
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-12 14:21:45 +0300

    feat: add search form for mobile

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,16 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <div className="search-form">
-      <div className="search-form__container" />
+      <div className="search-form__container">
+        <div>
+          <h1 className="search-form__title">Что творится в мире?</h1>
+          <p className="search-form__description">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+        </div>
+        <form>
+          <input className="search-form__input" placeholder="Природа" required />
+          <button className="search-form__submit-button" type="submit">Искать</button>
+        </form>
+      </div>
     </div>
   );
 }
