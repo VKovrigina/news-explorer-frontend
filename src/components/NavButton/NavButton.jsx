ca27fa3bad523fa86aa903f38befea8df502cbd6
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-17 11:26:57 +0300

    feat: add navigation style

---
+++ src/components/NavButton/NavButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavButton.css';
+
+function NavButton() {
+  return (
+    <button type="button" className="nav-button">Авторизоваться</button>
+  );
+}
+
+export default NavButton;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import NavButton from '../NavButton/NavButton';
 
 function Navigation() {
   return (
     <nav className="navigation">
       <Link to="/" className="navigation__link">Главная</Link>
+      {/* <Link to="/" className="navigation__link">Сохраненные статьи</Link>
       <Link to="/" className="navigation__link">Сохраненные статьи</Link>
-      <button type="button" className="navigation__button">Авторизоваться</button>
+      <Link to="/" className="navigation__link">Сохраненные статьи</Link> */}
+      <NavButton />
     </nav>
 
   );
