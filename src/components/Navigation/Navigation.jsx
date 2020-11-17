4bcf69848dfbcbc65461f5f4ab2df27227d8b92e
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-17 12:34:24 +0300

    feat: add active link style in jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Navigation.css';
 import NavButton from '../NavButton/NavButton';
 
 function Navigation({ isLoggedIn, setIsLoggedIn }) {
+  const location = useLocation();
   return (
     <nav className="navigation">
-      <Link to="/" className="navigation__link">Главная</Link>
-      {isLoggedIn && <Link to="/" className="navigation__link">Сохраненные статьи</Link>}
+      <Link
+        to="/"
+        className={`navigation__link navigation__link_white ${location.pathname === '/' ? 'navigation__link_active_white' : ''}`}
+      >
+        Главная
+      </Link>
+      {isLoggedIn
+      && (
+      <Link
+        to="/"
+        className="navigation__link navigation__link_white"
+      >
+        Сохраненные статьи
+      </Link>
+      )}
       <NavButton handlerClick={setIsLoggedIn} isLoggedIn={isLoggedIn} />
     </nav>
 
