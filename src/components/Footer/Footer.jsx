a729604c60a6edea0918d481714b28f440a658ab
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-18 13:52:03 +0300

    feat: add adaptive for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,29 +7,33 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
+        {/* Текстовые ссылки */}
         <div className="footer__links">
-          <ul className="footer__ul footer__ul_type_links">
-            <li className="footer__li">
-              <a className="footer__link" href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">Главная</a>
-            </li>
-            <li className="footer__li">
-              <a className="footer__link" href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__socials">
-          <ul className="footer__ul footer__ul_type_socials">
-            <li className="footer__li">
-              <a href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">
-                <img alt="GitHub" src={GitHub} />
-              </a>
-            </li>
-            <li className="footer__li">
-              <a href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">
-                <img alt="Facebook" src={Facebook} />
-              </a>
-            </li>
-          </ul>
+          <div>
+            <ul className="footer__ul footer__ul_type_links">
+              <li className="footer__li">
+                <a className="footer__link" href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">Главная</a>
+              </li>
+              <li className="footer__li">
+                <a className="footer__link" href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+              </li>
+            </ul>
+          </div>
+          {/* Ссылки - иконки */}
+          <div>
+            <ul className="footer__ul footer__ul_type_socials">
+              <li>
+                <a href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">
+                  <img className="footer__socials-icon" alt="GitHub" src={GitHub} />
+                </a>
+              </li>
+              <li>
+                <a href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">
+                  <img className="footer__socials-icon" alt="Facebook" src={Facebook} />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <p className="footer__copyright">&#169;Supersite, Powered by News API</p>
       </div>
