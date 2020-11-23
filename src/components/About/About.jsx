b4aebe268f52417f26b3a10ab6564a47c8bbb8d3
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-23 11:10:12 +0300

    feat: add text about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,8 +9,8 @@ function About() {
         <img className="about__author-photo" alt="Фото автора" src={AuthorPhoto} />
         <div className="about__content">
           <h2 className="about__title">Об авторе</h2>
-          <p className="about__paragraph">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.</p>
-          <p className="about__paragraph">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
+          <p className="about__paragraph">Привет! Меня зовут Варя, и я начинающий frontend-developer. Знаю как работать с JavaScript, CSS, ReactJS, NodeJS, MongoDB :)</p>
+          <p className="about__paragraph">Во время обучения в Яндекс.Практикум на себе прочувствовала фразу: &quot;The more I learn, the more I realise how much i don`t know &quot;. Не собираюсь останавливаться в изучении новых технологий :)</p>
         </div>
       </div>
     </section>
