030ae2c75c8985a3e85b947a295d7aed524923b6
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-23 11:15:30 +0300

    fix: link in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 import GitHub from '../../images/GitHub.svg';
 import Facebook from '../../images/Facebook.svg';
 
@@ -11,7 +12,7 @@ function Footer() {
         <div className="footer__links">
           <ul className="footer__ul footer__ul_type_links">
             <li className="footer__li">
-              <a className="footer__link" href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">Главная</a>
+              <Link className="footer__link" to="/">Главная</Link>
             </li>
             <li className="footer__li">
               <a className="footer__link" href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
@@ -20,7 +21,7 @@ function Footer() {
           {/* Ссылки - иконки */}
           <ul className="footer__ul footer__ul_type_socials">
             <li>
-              <a href="https://praktikum.yandex.ru/web/" target="_blank" rel="noreferrer">
+              <a href="https://github.com/VKovrigina" target="_blank" rel="noreferrer">
                 <img className="footer__socials-icon" alt="GitHub" src={GitHub} />
               </a>
             </li>
