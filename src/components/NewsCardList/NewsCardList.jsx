c50ea7d1110da03a50464295bf711d5b6a2fc059
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-28 17:27:04 +0300

    fix: key in articles

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './NewsCardList.css';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
 import NotFound from '../NotFound/NotFound';
 import NewsCard from '../NewsCard/NewsCard';
 import Preloader from '../Preloader/Preloader';
@@ -19,7 +18,7 @@ function NewsCardList({
             <NewsCard
               isLoggedIn={isLoggedIn}
               isMainPage={isMainPage}
-              key={uuidv4()}
+              key={[card.link, card.title, card.text]}
               {...card}
             />
           ))}
