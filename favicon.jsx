38a7c3f8126aafc370f9a7d33942c11055a77c02
vk <kovrigina149@gmail.com>
2022-01-21 16:23:46 +0300

    Updates

---
+++ favicon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="none"
+      viewBox="0 0 32 32"
+    >
+      <rect width="32" height="32" fill="#347EFF" rx="16"></rect>
+      <path
+        fill="#fff"
+        d="M21.883 10.625V12l-1.219.234V22h-2.273l-4.594-7.656-.047.008v6.046l1.219.235V22h-4.711v-1.367l1.21-.235v-8.164L10.259 12v-1.375h3.492l4.594 7.656.047-.008v-6.039L17.17 12v-1.375h4.712z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
